0f7ae488d5323f21bb95ae4a34e5a4546bc61c9c
Rafi Haidar <rafihaidararibawa@gmail.com>
2022-12-06 20:04:24 +0700

    Responsi

---
+++ src/app/data-tambah/data-tambah.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { ApiService } from '../api.service';
+import { Http } from '@capacitor-community/http';
+import { Alert } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-data-tambah',
+  templateUrl: './data-tambah.page.html',
+  styleUrls: ['./data-tambah.page.scss'],
+})
+export class DataTambahPage implements OnInit {
+  tanggal: any;
+  peminjaman: any;
+  lama: any;
+  total: any;
+  nama: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public _apiService: ApiService,
+    private alertController: AlertController,
+    public loadingController: LoadingController
+  ) {}
+
+  ngOnInit() {}
+
+  addData() {
+    let url = this._apiService.apiURL() + '/tambah.php';
+    Http.request({
+      method: 'POST',
+      url: url,
+      headers: { 'Content-Type': 'application/json' },
+      data: {
+        tanggal: this.tanggal,
+        peminjaman: this.peminjaman,
+        lama: this.lama,
+        total: this.total,
+        nama: this.nama,
+      },
+    }).then(
+      (data) => {
+        this.tanggal = '';
+        this.peminjaman = '';
+        this.lama = '';
+        this.total = '';
+        this.nama = '';
+        this.alertController
+          .create({
+            header: 'Notifikasi',
+            message: 'Berhasil Input Data',
+            buttons: ['OK'],
+          })
+          .then((res) => {
+            res.present();
+          });
+        this.router.navigateByUrl('/home');
+      },
+      (error) => {
+        this.alertController
+          .create({
+            header: 'Notifikasi',
+            message: 'Gagal Input Data',
+            buttons: ['OK'],
+          })
+          .then((res) => {
+            res.present();
+          });
+      }
+    );
+  }
+}
